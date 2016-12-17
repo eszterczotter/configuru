d3e89a9c1cb6bef9ee775dcb4dab1e8229050e30
Eszter Czotter <eszter.czotter@gmail.com>
2016-12-17 14:07:09 +0100

    Add replace functionality

--- src/Commands/Build/Handler.php
+++ src/Commands/Build/Handler.php
@@ -2,9 +2,37 @@
 
 namespace Configuru\Commands\Build;
 
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Yaml\Parser;
+
 class Handler
 {
+    /**
+     * @var Finder
+     */
+    private $finder;
+    /**
+     * @var Yaml
+     */
+    private $yaml;
+
+    public function __construct(Finder $finder, Parser $yaml)
+    {
+        $this->finder = $finder;
+        $this->yaml = $yaml;
+    }
+
     public function handle(Command $command)
     {
+        $config = $this->yaml->parse(file_get_contents(getcwd() . '/configuru.yml'));
+        $replace = [];
+        foreach ($config['replace'] ?? [] as $key => $value) {
+            $replace[":({$key})"] = $value;
+        }
+        $files = $this->finder->files()->exclude('tests')->in(getcwd())->name('*.guru');
+        foreach ($files as $file) {
+            $path = preg_replace('/\.guru$/', '', $file->getRealPath());
+            file_put_contents($path, strtr($file->getContents(), $replace));
+        }
     }
 }
--- tests/acceptance/BuildCommandCest.php
+++ tests/acceptance/BuildCommandCest.php
@@ -7,6 +7,6 @@ class BuildCommandCest
         $I->copyDir(__DIR__ . '/../_dummy', __DIR__ . '/../../');
         $I->runShellCommand("bin/configuru build");
         $I->openFile(__DIR__ . '/../../example/config.yml');
-        //$I->seeInThisFile('correct value');
+        $I->seeInThisFile('correct value');
     }
 }
