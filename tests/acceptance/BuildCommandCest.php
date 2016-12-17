f3ef6e7e8669c3591f80d424c1a801567f67f960
Eszter Czotter <eszter.czotter@gmail.com>
2016-12-17 09:50:03 +0100

    Add example files

--- tests/acceptance/BuildCommandCest.php
+++ tests/acceptance/BuildCommandCest.php
@@ -2,8 +2,11 @@
 
 class BuildCommandCest
 {
-    function updateConfiguration(AcceptanceTester $I)
+    function buildConfiguru(AcceptanceTester $I)
     {
+        $I->copyDir(__DIR__ . '/../_dummy', __DIR__ . '/../../');
         $I->runShellCommand("bin/configuru build");
+        $I->openFile(__DIR__ . '/../../example/config.yml');
+        //$I->seeInThisFile('correct value');
     }
 }
