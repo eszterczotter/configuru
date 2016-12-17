54def10eea7014cbf9d9d36a374aa396ba09062f
Eszter Czotter <eszter.czotter@gmail.com>
2016-12-17 11:29:06 +0100

    Add a service container

---
+++ tests/unit/Service/ContainerSpec.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace unit\Configuru\Service;
+
+use Configuru\Service\Container;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ContainerSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(Container::class);
+    }
+}
