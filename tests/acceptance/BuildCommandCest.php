1c32569880ef3745cca9c3a99b6b039d8100b327
Eszter Czotter <eszter.czotter@gmail.com>
2016-12-24 07:58:58 +0100

    Rename dummy folder to files

--- tests/acceptance/BuildCommandCest.php
+++ tests/acceptance/BuildCommandCest.php
@@ -17,7 +17,7 @@ class BuildCommandCest
 
     function buildConfiguru(AcceptanceTester $I)
     {
-        $I->copyDir(__DIR__ . '/../_dummy', __DIR__ . '/../../');
+        $I->copyDir(__DIR__ . '/../_files', __DIR__ . '/../../');
         $I->runShellCommand("bin/configuru build example");
         $I->openFile(__DIR__ . '/../../example/config.yml');
         $I->seeInThisFile('correct value');
